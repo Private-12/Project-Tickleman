db1b26995a96a514cb8b70a52d50a9ccd05350ac
Pranshu <sharmapranshu600@gmail.com>
2025-06-18 20:32:25 +0100

    Centralized text

--- Stew.js
+++ Stew.js
@@ -1,8 +1,8 @@
 function check() {
    const txt=document.getElementById("txt");
    if (txt.value == "and stuff like that.") {
-      document.getElementById("result").innerHTML = "You are correct!";
-      document.getElementById("restxt").style.display = 'block';
+      document.getElementById("restxt").innerHTML = "You are correct!";
+      document.getElementById("result").style.display = 'block';
    } else {
       document.getElementById("restxt").innerHTML = "Try again.";
       document.getElementById("result").style.display = 'block';
